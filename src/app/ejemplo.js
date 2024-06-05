a05f4a0f894500b05ede0f53e6177f18333165cb
Barbara796 <barbara6.dossantos@gmail.com>
2024-06-05 16:45:00 -0300

    aula 1

---
+++ src/app/ejemplo.js
@@ -0,0 +1,23 @@
+const podFront = "PodFront"
+const podBack = "PodBack"
+const podData = "PodData"
+
+//array o arreglos
+
+const podcast = ["PodFront", "PodBack", "PodData"]
+
+//los indexes
+/* 0 - podFront
+1 - podBack
+2 - podData 
+
+*/
+
+//cómo saber el valor de un elemento de un arreglo 
+console.log(podcast[0])
+
+//para saber el tamaño del arreglo
+console.log(podcast.length)
+
+//para saber cuál es el último valor del arreglo
+console.log(podcast[podcast.length-1])
