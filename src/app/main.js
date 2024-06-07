7469328ce2541f22b12537b52fbd4ec8cf16bb4f
Barbara796 <barbara6.dossantos@gmail.com>
2024-06-07 14:07:09 -0300

    aula3

---
+++ src/app/main.js
@@ -0,0 +1,12 @@
+import { podcasts } from "./datos.js";
+import { generateCard, generatePlayer } from "./generator.js";
+
+const parentElement = document.querySelector('[data-indicados]');
+
+
+//forEach
+// Iteramos sobre el arreglo de podcasts y agregamos cada tarjeta al DOM
+ podcasts.forEach((podcast) => {
+  const card = generateCard(podcast);
+  parentElement.appendChild(card);
+}); 
